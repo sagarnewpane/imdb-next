02ba25706e10144c67936ac061b03a44ddd19ebf
sagarnewpane <sdreamnewpane@gmail.com>
2024-06-03 22:35:18 +0545

    added header

---
+++ src/app/components/MenuItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+function MenuItem({title,address,Icon}) {
+  return (
+    <Link href={address} className='hover:text-amber-500'>
+        <Icon className="text-2xl sm:hidden" />
+        <p className='uppercase hidden sm:inline text-sm'>{title}</p>
+    </Link>
+
+  )
+}
+
+export default MenuItem
