33c6d6bfc629ef6d3bf3141f420c06fc6a063da0
sagarnewpane <sdreamnewpane@gmail.com>
2024-06-04 07:45:06 +0545

    created about

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolore in ipsam! Quaerat earum illo voluptate sed qui nesciunt perspiciatis sunt quis, asperiores vero maiores error quasi est molestias consequatur tempore dolorum, dolore recusandae ea velit temporibus? Praesentium, rem modi?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse repudiandae ullam blanditiis sint obcaecati iusto mollitia vero hic amet. Inventore corporis dolores, tenetur nesciunt quidem illo sed earum architecto laboriosam aut, ex molestiae non deserunt cumque impedit! Optio, dolores ad, sequi ipsa distinctio similique illum dolorum suscipit tempore, eveniet quasi.
+        </p>
+    </div>
+  )
+}
+
+export default About
